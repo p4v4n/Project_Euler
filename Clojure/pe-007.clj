0f31afcf610186de41b78cf3641574f81018cd74
p4v4n <pavan.barcelona@gmail.com>
2017-07-14 13:55:05 +0530

    Add pe-007

---
+++ Clojure/pe-007.clj
@@ -0,0 +1,16 @@
+(defn is-prime-number? [n]
+    (loop [x (int (Math/floor (Math/pow n 0.5)))]
+        (if (= x 1)
+            true
+            (if (= 0 (rem n x))
+                false
+                (recur (dec x))))))
+
+(defn nth-prime [n]
+	(->> (range)
+		 (drop 2)
+		 (filter is-prime-number?)
+		 (take n)
+		 (last)))
+
+(println (nth-prime 10001))
